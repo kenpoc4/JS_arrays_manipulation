53a71e8ba960cfb3dd90d42482b13cb0836ff519
Kenny Poncio <kennyponcio07@gmail.com>
2021-12-27 21:09:13 -0600

    Reduce class

---
+++ server/07-some.js
@@ -0,0 +1,27 @@
+const numbers = [1, 2, 3, 4];
+
+const rta = numbers.some( item => item % 2 === 0 );
+console.log(rta);
+
+const dates = [
+    {
+        startDate: new Date(2021, 1, 1, 10),
+        endDate: new Date(2021, 1, 1, 11),
+        title: "Cita de trabajo",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 15),
+        endDate: new Date(2021, 1, 1, 15, 30),
+        title: "Cita con mi jefe",
+    },
+    {
+        startDate: new Date(2021, 1, 1, 20),
+        endDate: new Date(2021, 1, 1, 21),
+        title: "Cena",
+    },
+];
+const newAppointment = {
+    startDate: new Date(2021, 1, 1, 8),
+    endDate: new Date(2021, 1, 1, 9, 30),
+};
+const areIntervalsOverlapping = require('date-fns/areIntervalsOverlapping');
